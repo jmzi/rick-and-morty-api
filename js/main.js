c8c8d03fae9557b5c6ab399f18e11c1b040a94a7
Jimmy <jhimzmabana@gmail.com>
2022-05-30 17:06:30 +0800

    favicon & other details

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 $.getJSON('https://rickandmortyapi.com/api/character/', function(data) {
 
+       
         var text = "";
         
         for (let i = 0; i < data.results.length; i++) {
+
+            var typ = data.results[i].type;
+
+            if(typ == "") {
+                typ = "Unknown";
+            }
+
             text += "<div class='item'><img src='" + data.results[i].image + "'/>" +
                 "<h3>" + data.results[i].id + ".) " + data.results[i].name + "</h3> " +
-                "Status: " + data.results[i].status + " <br>" + 
-                " Gender: " + data.results[i].gender + "</div>";
+                "Status: " + data.results[i].status + "<br>" + 
+                "Gender: " + data.results[i].gender + "<br>" +
+                "Species: " + data.results[i].species + "<br>" +
+                "Origin: " + data.results[i].origin.name + "<br>" +
+                "Location: " + data.results[i].location.name + "<br>" +
+                "Type: " + typ + "</div>";
         }
 
         $(".mycontainer").html(text);
